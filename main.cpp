c1a6e0e3802627030489411d8c3e977480010a49
lab03(KomarevtsevER) <(komarevtsevyr@mpei.ru)>
2022-04-18 17:15:47 +0300

    добавление SVG

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
+#include <math.h>
+#include <conio.h>
+#include <string.h>
 #include <vector>
+#include<windows.h>
+#pragma hdrstop
+using namespace std;
+
+
 
 using namespace std;
 
@@ -91,6 +99,60 @@ const size_t SCREEN_WIDTH = 80;
     }
 }
 
+void
+svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void
+svg_end() {
+    cout << "</svg>\n";
+}
+
+void
+svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='"<< baseline << "'>"<< text <<"</text>" <<endl;
+}
+
+void svg_rect(double x, double y, double width, double height,string stroke, string fill){
+
+    cout<<"<rect x='" << x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />" <<endl;
+}
+
+
+void
+show_histogram_svg(const vector<size_t>& bins)
+{
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+    const string stroke = "black";
+    const string fill = "#3CB371";
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+    double top = 0;
+    for (size_t bin : bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        const double text_wid= IMAGE_WIDTH-bin_width-30;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"black", "#3CB371");
+        top += BIN_HEIGHT;
+    }
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
+    svg_end();
+}
+
+
 int main()
 {
     // Ввод данных
@@ -107,6 +169,6 @@ int main()
     // Обработка данных
     const auto bins = make_histogram(numbers, bin_count);
     // Вывод данных
-    show_histogram(bins);
+    show_histogram_svg(bins);
     return 0;
 }
