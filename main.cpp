eec23943bc5bd2d0eace170eb9d04173ba650180
lab03(KomarevtsevER) <(komarevtsevyr@mpei.ru)>
2022-05-30 15:53:25 +0300

    code:добавление bool prompt

--- main.cpp
+++ main.cpp
@@ -21,17 +21,20 @@ input_numbers(istream& in, size_t count)
 }
 
 Input
-read_input(istream& in) {
+read_input(istream& in, bool prompt) {
     Input data;
 
-    cerr << "Enter number count: ";
+    if (prompt)
+        cerr << "Enter number count: ";
     size_t number_count;
     in >> number_count;
 
-    cerr << "Enter numbers: ";
+    if (prompt)
+        cerr << "Enter numbers: ";
     data.numbers = input_numbers(in, number_count);
 
-    cerr << "Enter bin count: ";
+    if (prompt)
+        cerr << "Enter bin count: ";
     in >> data.bin_count;
 
     return data;
@@ -103,11 +106,10 @@ show_histogram( const auto& bins)
 int main()
 {
     // Ввод данных
-    Input data;
-    data=read_input(cin);
-
+    const auto input = read_input(cin, true);
     // Обработка данных
-    const auto bins = make_histogram(data);
+
+    const auto bins = make_histogram(input);
 
     // Вывод данных
     show_histogram_svg(bins);
