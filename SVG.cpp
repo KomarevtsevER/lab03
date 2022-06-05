fc7cfce97a75de43fb14eb977efbd690cf6ad028
lab03(KomarevtsevER) <(komarevtsevyr@mpei.ru)>
2022-06-06 00:47:03 +0300

    Revert "code:добавление printf"
    This reverts commit a56e52efe24e23297d5282e558ddb4806cc56120.

--- SVG.cpp
+++ SVG.cpp
@@ -1,5 +1,7 @@
 #include "SVG.h"
-void svg_begin(double width, double height) {
+void
+svg_begin(double width, double height)
+{
     cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
     cout << "<svg ";
     cout << "width='" << width << "' ";
@@ -7,17 +9,29 @@ void svg_begin(double width, double height) {
     cout << "viewBox='0 0 " << width << " " << height << "' ";
     cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
-void svg_end() {
+
+void
+svg_end()
+{
     cout << "</svg>\n";
 }
-void svg_rect(double x, double y, double width, double height, string stroke, string fill){
-    cout << "<rect x='" << x <<"' y='" << y << "' width='" << width <<"' height='" << height <<"' stroke='"<< stroke <<"' fill='#"<< fill <<"' />";
-}
-void svg_text(double left, double baseline, string text) {
-    cout << "<text x='" << left << "' y='" << baseline << "'>" << text <<"</text>";
+
+void
+svg_text(double left, double baseline, string text)
+{
+    cout << "<text x='" << left << "' y='"<< baseline << "'>"<< text <<"</text>" <<endl;
 }
 
-void show_histogram_svg(const vector<size_t>& bins) {
+void svg_rect(double x, double y, double width, double height,string stroke, string fill)
+{
+
+    cout<<"<rect x='" << x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />" <<endl;
+}
+
+
+void
+show_histogram_svg(const vector<size_t>& bins)
+{
     const auto IMAGE_WIDTH = 400;
     const auto IMAGE_HEIGHT = 300;
     const auto TEXT_LEFT = 20;
@@ -25,26 +39,18 @@ void show_histogram_svg(const vector<size_t>& bins) {
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
-    const auto MAX_ASTERISK = IMAGE_WIDTH - TEXT_WIDTH;
-    size_t max_count = 0;
-    for (size_t bin : bins) {
-        if (bin * BLOCK_WIDTH > max_count) {
-            max_count = bin * BLOCK_WIDTH;
-        }
-    }
-    const bool scaling_needed = max_count > MAX_ASTERISK;
+    const string stroke = "black";
+    const string fill = "#3CB371";
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
-    for (size_t bin : bins) {
-        size_t bin_scale = bin;
-        if (scaling_needed) {
-            const double scaling_factor = (double)MAX_ASTERISK / max_count;
-            bin_scale = (size_t)(bin * scaling_factor);
-        }
-        const double bin_width = BLOCK_WIDTH * bin_scale;
+    for (size_t bin : bins)
+    {
+        const double bin_width = BLOCK_WIDTH * bin;
+        const double text_wid= IMAGE_WIDTH-bin_width-30;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"black", "#3CB371");
         top += BIN_HEIGHT;
     }
+    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
     svg_end();
 }
