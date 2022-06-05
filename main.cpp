2d3030e87ec25dd7d7937efcb071bec2b5cae079
lab03(KomarevtsevER) <(komarevtsevyr@mpei.ru)>
2022-06-05 21:22:22 +0300

    code:добавление вывода ошибки

--- main.cpp
+++ main.cpp
@@ -111,6 +111,10 @@ int main(int argc, char* argv[]) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if(res){
+                cerr<<" Error text  = "<<curl_easy_strerror(res);
+                exit(1);
+            }
             curl_easy_cleanup(curl);
         }
         return 0;
