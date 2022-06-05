22b6cfa5f2d76d3cbc9e007899e8c5fb1f83fb5b
lab03(KomarevtsevER) <(komarevtsevyr@mpei.ru)>
2022-06-06 02:53:05 +0300

    code:добавление версии Windows

--- SVG.cpp
+++ SVG.cpp
@@ -1,7 +1,5 @@
 #include "SVG.h"
-void
-svg_begin(double width, double height)
-{
+void svg_begin(double width, double height) {
     cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
     cout << "<svg ";
     cout << "width='" << width << "' ";
@@ -9,29 +7,17 @@ svg_begin(double width, double height)
     cout << "viewBox='0 0 " << width << " " << height << "' ";
     cout << "xmlns='http://www.w3.org/2000/svg'>\n";
 }
-
-void
-svg_end()
-{
+void svg_end() {
     cout << "</svg>\n";
 }
-
-void
-svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='"<< baseline << "'>"<< text <<"</text>" <<endl;
+void svg_rect(double x, double y, double width, double height, string stroke, string fill){
+    cout << "<rect x='" << x <<"' y='" << y << "' width='" << width <<"' height='" << height <<"' stroke='"<< stroke <<"' fill='#"<< fill <<"' />";
+}
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text <<"</text>";
 }
 
-void svg_rect(double x, double y, double width, double height,string stroke, string fill)
-{
-
-    cout<<"<rect x='" << x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<<stroke<<"' fill='"<<fill<<"' />" <<endl;
-}
-
-
-void
-show_histogram_svg(const vector<size_t>& bins)
-{
+void show_histogram_svg(const vector<size_t>& bins) {
     const auto IMAGE_WIDTH = 400;
     const auto IMAGE_HEIGHT = 300;
     const auto TEXT_LEFT = 20;
@@ -39,18 +25,26 @@ show_histogram_svg(const vector<size_t>& bins)
     const auto TEXT_WIDTH = 50;
     const auto BIN_HEIGHT = 30;
     const auto BLOCK_WIDTH = 10;
-    const string stroke = "black";
-    const string fill = "#3CB371";
+    const auto MAX_ASTERISK = IMAGE_WIDTH - TEXT_WIDTH;
+    size_t max_count = 0;
+    for (size_t bin : bins) {
+        if (bin * BLOCK_WIDTH > max_count) {
+            max_count = bin * BLOCK_WIDTH;
+        }
+    }
+    const bool scaling_needed = max_count > MAX_ASTERISK;
     svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
     double top = 0;
-    for (size_t bin : bins)
-    {
-        const double bin_width = BLOCK_WIDTH * bin;
-        const double text_wid= IMAGE_WIDTH-bin_width-30;
+    for (size_t bin : bins) {
+        size_t bin_scale = bin;
+        if (scaling_needed) {
+            const double scaling_factor = (double)MAX_ASTERISK / max_count;
+            bin_scale = (size_t)(bin * scaling_factor);
+        }
+        const double bin_width = BLOCK_WIDTH * bin_scale;
         svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT,"black", "#3CB371");
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT);
         top += BIN_HEIGHT;
     }
-    svg_text(TEXT_LEFT, TEXT_BASELINE, to_string(bins[0]));
     svg_end();
 }
--- main.cpp
+++ main.cpp
@@ -1,110 +1,89 @@
 #include <iostream>
-#include <math.h>
-#include <conio.h>
-#include <string.h>
 #include <vector>
 #include "histogram.h"
 #include "SVG.h"
-#include<windows.h>
-#pragma hdrstop
+#include <windows.h>
 using namespace std;
-
-
-
-using namespace std;
-
 vector<double>
-input_numbers(size_t count)
-{
+input_numbers(size_t count) {
     vector<double> result(count);
-    for (size_t i = 0; i < count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         cin >> result[i];
     }
     return result;
 }
-
 vector<size_t>
-make_histogram(const vector<double> &numbers, const size_t &bin_count)
-{
-
+make_histogram(const vector<double> &numbers, const size_t &bin_count){
     double min, max;
     find_minmax(numbers, min, max);
     vector<size_t> bins(bin_count, 0);
-    for (double number : numbers)
-    {
+    for (double number : numbers) {
         size_t bin = (size_t)(((number - min) / (max - min)) * bin_count);
-        if (bin == bin_count)
-        {
+        if (bin == bin_count) {
             bin--;
         }
         bins[bin]++;
     }
-    return bins;
+    return(bins);
 }
-
 void
-show_histogram( const auto& bins)
-{
+show_histogram_text(const vector <size_t> &bins){
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
-
     size_t max_count = 0;
-    for (size_t count : bins)
-    {
-        if (count > max_count)
-        {
-            max_count = count;
+    for (size_t bin : bins) {
+        if (bin > max_count) {
+            max_count = bin;
         }
     }
     const bool scaling_needed = max_count > MAX_ASTERISK;
-
-    for (size_t bin : bins)
-    {
-        if (bin < 100)
-        {
+    for (size_t bin : bins) {
+        if (bin < 100) {
             cout << ' ';
         }
-        if (bin < 10)
-        {
+        if (bin < 10) {
             cout << ' ';
         }
         cout << bin << "|";
-
         size_t height = bin;
-        if (scaling_needed)
-        {
+        if (scaling_needed) {
             const double scaling_factor = (double)MAX_ASTERISK / max_count;
             height = (size_t)(bin * scaling_factor);
         }
-
-        for (size_t i = 0; i < height; i++)
-
-        {
+        for (size_t i = 0; i < height; i++) {
             cout << '*';
         }
         cout << '\n';
     }
 }
 
+int
+main() {
+    DWORD info = GetVersion();
+    DWORD mask = 0x0000ffff;
+    DWORD version = info & mask;
+    DWORD platform = info >> 16;
+    DWORD version_minor = version >> 8;
+    DWORD version_major = version & 0x0000ff;
+    printf("Windows v%u.%u", version_major, version_minor);
 
+     if ((info & 0x40000000) == 0) {
+        DWORD build = platform;
+        printf(" (build %u)", build);
+    }
+    return 0;
 
-int main()
-{
     // Ввод данных
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
-
     cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count );
+    const auto numbers = input_numbers(number_count);
     size_t bin_count;
-    cerr << "Enter bin count: ";
+    cerr << "Enter column count: ";
     cin >> bin_count;
-
-    // Обработка данных
+    // Вывод гистограммы
     const auto bins = make_histogram(numbers, bin_count);
-    // Вывод данных
-    show_histogram_svg(bins);
+     show_histogram_svg(bins);
     return 0;
 }
